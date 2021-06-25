04889c2b1f977a07a19215b2683081ea1cb34186
Bill Spell <william.spell@vanderbilt.edu>
2021-06-24 21:34:38 -0500

    push

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ connection.on("error", console.error.bind(console, "mongo connection error"));
 
 // Original routes
 
-todoRoutes.route('/').get(function(req, res) {
+todoRoutes.route('/:user').get(function(req, res) {
     Todo.find(function(err, todos) {
         if (err) {
             console.log(err);
@@ -40,14 +40,14 @@ todoRoutes.route('/').get(function(req, res) {
     });
 });
 
-todoRoutes.route('/:id').get(function(req, res) {
+todoRoutes.route('/:user/:id').get(function(req, res) {
     let id = req.params.id;
     Todo.findById(id, function(err, todo) {
         res.json(todo);
     });
 });
 
-todoRoutes.route('/update/:id').post(function(req, res) {
+todoRoutes.route('/:user/update/:id').post(function(req, res) {
     Todo.findById(req.params.id, function(err, todo) {
         if (!todo)
             res.status(404).send("data is not found");
@@ -65,7 +65,7 @@ todoRoutes.route('/update/:id').post(function(req, res) {
     });
 });
 
-todoRoutes.route('/add').post(function(req, res) {
+todoRoutes.route('/:user/add').post(function(req, res) {
     let todo = new Todo(req.body);
     todo.save()
         .then(todo => {
@@ -76,7 +76,7 @@ todoRoutes.route('/add').post(function(req, res) {
         });
 });
 
-todoRoutes.route('/delete/:id').delete((req, res, next) => {
+todoRoutes.route('/:user/delete/:id').delete((req, res, next) => {
     Todo.findByIdAndRemove(req.params.id, (error, data) => {
       if (!data) {
         return next(error);
