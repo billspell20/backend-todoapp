9eacdfaeb47b8186af11203ac3d1c9a17006168a
Bill Spell <william.spell@vanderbilt.edu>
2021-07-12 22:10:57 -0500

    cors

--- app.js
+++ app.js
@@ -102,10 +102,6 @@ app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
 app.use('*', express.static(path.join(__dirname, "client", "build")))
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 module.exports = app;
