0e0977be276eeddeac67013470aff82034692fdc
Bill Spell <william.spell@vanderbilt.edu>
2021-07-12 22:20:23 -0500

    push

--- app.js
+++ app.js
@@ -102,7 +102,6 @@ app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+app.use('*', express.static(path.join(__dirname, "client", "build", "index.html")))
+
 module.exports = app;
