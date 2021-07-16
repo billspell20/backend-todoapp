5c03792ed12dc7c32afe359aa110ec66d3058e40
Bill Spell <william.spell@vanderbilt.edu>
2021-07-16 13:33:01 -0500

    cors config

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ connection.once('open', _ => {
   })
 connection.on("error", console.error.bind(console, "mongo connection error"));
 
-
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 // Original routes
 
